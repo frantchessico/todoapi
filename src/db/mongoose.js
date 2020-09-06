19dc79b1797b65bc8ad190c66bd94c14cc50a303
Francisco Inoque <tchessico@live.com>
2020-09-06 20:57:17 -0300

    confi database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
+const prodUrl = "mongodb+srv://francisco-savanapoint:Luisa@jaime1996@cluster0.jao6i.mongodb.net/TodoApi?retryWrites=true&w=majority"
 const urlDB = 'mongodb://localhost/todoapi'
-mongoose.connect(urlDB, {
+mongoose.connect(prodUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 
